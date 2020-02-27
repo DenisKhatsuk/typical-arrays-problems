8dd939e0cf41b19a4c90c9d520c9b16280bca2d3
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-02-27 18:41:30 +0300

    feat: add arrays functions [min, max and average value search]

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array||!array.length) return 0;
+  let size = array.length;
+  let min = 0;
+  
+  for (let i = 0; i < size; i++) {
+    min = array[i] < min ? array[i] : min;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array||!array.length) return 0;
+    let size = array.length;
+    let max = 0;
+    for (let i = 0; i < size; i++) {
+      max = array[i] > max ? array[i] : max;
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array||!array.length) return 0;
+    let size = array.length;
+    let sum = 0;
+    for (let i = 0; i < size; i++) {
+      sum += array[i];
+    }
+    return (sum / size);
 }
